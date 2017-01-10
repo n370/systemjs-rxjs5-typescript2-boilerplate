5409c7cc77e906aa36a8aab6280e632b3c0f4f2b
Dylson Valente Neto <ama@n370.info>
2017-01-10 17:25:10 -0300

    Init

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { Observable } from "@reactivex/rxjs";
+
+const startButton = document.querySelector('#start');
+
+const start$ = Observable.fromEvent(startButton, 'click');
+const interval$ = Observable.interval(1000);
+
+const startInterval$ = start$.switchMapTo(interval$);
+
+startInterval$.subscribe((x) => console.log(x));
